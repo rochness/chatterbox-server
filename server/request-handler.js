5aa835938f16916032349144fd25485501c1e824
rochness <rochelle.y.lee@gmail.com>
2015-10-26 21:06:42 -0700

    Passed all tests.

--- server/request-handler.js
+++ server/request-handler.js
@@ -11,6 +11,9 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
+var querystring = require('querystring');
+var util = require('util');
+var data = { results: [] };
 
 var requestHandler = function(request, response) {
   // Request and Response come from node's http module.
@@ -27,6 +30,7 @@ var requestHandler = function(request, response) {
   // Adding more logging to your server can be an easy way to get passive
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
+  
   console.log("Serving request type " + request.method + " for url " + request.url);
 
   // The outgoing status.
@@ -39,11 +43,43 @@ var requestHandler = function(request, response) {
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
+  
   headers['Content-Type'] = "text/plain";
+  // if ( response.contentType === 'application/json' ) {
+  //   headers['Content-Type'] = 'application/json';
+  // } else {
+  //   headers['Content-Type'] = "text/plain";
+  // }
+
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
+
+  if ( request.method === 'GET' ) {
+    console.log(request.url)
+    if( request.url === '/classes/room1' ){
+      response.writeHead(statusCode, headers);
+      response.end(JSON.stringify(data));
+    } else if ( request.url === '/classes/messages' ) {
+      response.end(JSON.stringify(data));
+    } else {
+      statusCode = 404;
+      response.writeHead(statusCode, headers);
+      response.end();
+    }
+  } else if ( request.method === 'POST' ) {
+    statusCode = 201;
+    var decodedBody;
+    var fullBody = '';
+    response.writeHead(statusCode, headers);
+    request.on('data', function (chunk) {
+      fullBody += chunk.toString();
+    });  
+    request.on('end', function () {
+      data.results.push(JSON.parse(fullBody.toString()));
+    });
+    response.end("post request success!");
+  }
 
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
@@ -52,7 +88,6 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end("Hello, World!");
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -70,7 +105,6 @@ var defaultCorsHeaders = {
   "access-control-allow-headers": "content-type, accept",
   "access-control-max-age": 10 // Seconds.
 };
-module.exports = function () {
-  return requestHandler;
-}
+
+module.exports.requestHandler = requestHandler;
 
